5696c138c3bb94885b4c6c0238625e3e0d3b8b6f
ccllarsaustero <119422419+ccllarsaustero@users.noreply.github.com>
2023-05-02 11:06:51 -0400

    initial commit

---
+++ dashboard/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Product
+from django.contrib.auth.models import Group
+
+admin.site.site_header = 'SarsInventory Dashboard'
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name','category','quantity')
+    list_filter = ['category']
+
+# Register your models here.
+
+admin.site.register(Product,ProductAdmin)
+# admin.site.unregister(Group)
+
+#test git hub
