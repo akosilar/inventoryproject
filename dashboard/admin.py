1aa7161a24323476d57ca6dcbbabcbc58a7acdf2
ccllarsaustero <119422419+ccllarsaustero@users.noreply.github.com>
2023-05-02 13:58:54 -0400

    added registration page
    used crispy forms

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Order
 from django.contrib.auth.models import Group
 
 admin.site.site_header = 'SarsInventory Dashboard'
@@ -11,6 +11,7 @@ class ProductAdmin(admin.ModelAdmin):
 # Register your models here.
 
 admin.site.register(Product,ProductAdmin)
+admin.site.register(Order)
 # admin.site.unregister(Group)
 
 #test git hub
